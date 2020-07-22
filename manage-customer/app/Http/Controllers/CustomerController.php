3dc4ef289b1b2361087690a569069a48c3dd44de
huynhngocthang <huynhngocthang789@gmail.com>
2020-07-22 11:33:25 +0700

    MANAGE_CUSTOMER

---
+++ manage-customer/app/Http/Controllers/CustomerController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Customer;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+
+class CustomerController extends Controller
+{
+    public function index() {
+        $customers = Customer::all() ;
+
+        return view('customers.list', compact('customers')) ;
+    }
+
+    public function create() {
+
+        return view('customers.create') ;
+    }
+
+    public function store(Request $request) {
+      $customer = new Customer() ;
+      $customer->name = $request->input('name') ;
+      $customer->email = $request->input('email') ;
+      $customer->dob = $request->input('dob') ;
+      $customer->save() ;
+
+      session()->flash('success', 'tạo mới khách hàng thành công');
+
+      return redirect()->route('customers.index') ;
+
+    }
+
+    public function edit($id) {
+        // hiển thị form và dữ liệu khách hàng cần sửa
+
+        $customer = Customer::findOrFail($id) ;
+        return view('customers.edit', compact('customer')) ;
+    }
+
+    public function update(Request $request, $id) {
+        //thực hiện tác vị sửa
+
+        $customer = Customer::findOrFail($id) ;
+        $customer->name = $request->input('name') ;
+         $customer->email = $request->input('email') ;
+        $customer->dob = $request->input('dob') ;
+        $customer->save() ;
+        //dùng session để đưa ra thông báo
+        Session::flash('success', 'Cập nhật khách hàng thành công');
+      //cap nhat xong quay ve trang danh sach khach hang
+      return redirect()->route('customers.index');
+
+    }
+
+    public function destroy($id) {
+        //xóa khách hàng
+        $customer = Customer::findOrFail($id) ;
+        $customer->delete() ;
+
+        session()->flash('success', 'Xóa khách hàng thành công');
+
+        return redirect()->route('customers.index') ;
+
+    }
+}
--- manage-customer/routes/web.php
+++ manage-customer/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Routing\RouteGroup;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+
+
+Route::group(['prefix' => 'customers'], function () {
+    Route::get('/','CustomerController@index')->name('customers.index') ;
+    Route::get('/create', 'CustomerController@create')->name('customers.create') ;
+    Route::post('/create','CustomerController@store')->name('customers.store') ;
+    Route::get('/{id}/edit','CustomerController@edit')->name('customers.edit') ;
+    Route::post('/{id}/edit','CustomerController@update')->name('customers.update') ;
+    Route::get('/{id}/destroy', 'CustomerController@destroy')->name('customers.destroy');
+
 });
